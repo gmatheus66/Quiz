f3c8b51d059c00931a0b1a87317284730cd0f0e4
gmatheus66 <gmatheus66@hotmail.com>
2018-10-31 02:17:11 -0300

    preparando o projeto para eventuais mudanças

--- index.js
+++ index.js
@@ -2,51 +2,55 @@
 var botao1 = document.getElementById("btn1");
 var botao2 = document.getElementById("btn2");
 var botao3 = document.getElementById("btn3");
-var gabarito =[];
+var label1 = document.getElementById("label1");
+var label2 = document.getElementById("label2");
+var label3 = document.getElementById("label3");
+var gabarito = [];
 var opcoes = [];
 var letra;
-var i;
 
-botao1.addEventListener('click', function(){
-   letra = "a";
-   opcoes.push(letra);
-   console.log(letra);
+
+botao1.addEventListener('click', function () {
+    letra = "a";
+    opcoes.push(letra);
+    console.log(letra);
 })
 
-botao2.addEventListener('click', function(){
+botao2.addEventListener('click', function () {
     letra = "b";
     opcoes.push(letra);
     console.log(letra);
- })
+})
 
- botao3.addEventListener('click', function(){
+botao3.addEventListener('click', function () {
     letra = "c";
     opcoes.push(letra);
     console.log(letra);
- })
+})
 
 
-function Opcao(){
+
+function Opcao() {
     var input = document.getElementById('a');
     //var div =  document.getElementById("demo");
     //div.innerHTML = input.value;
     var letra = input.value;
-    switch(letra){
+    switch (letra) {
         case 'a':
-        return opcoes[0] = 'a';
+            return opcoes[0] = 'a';
         case 'b':
-        return opcoes[1] = 'b';
+            return opcoes[1] = 'b';
         case 'c':
-        return opcoes[2] = 'c';
+            return opcoes[2] = 'c';
         default:
-        return "opcao errada";
+            return "opcao errada";
     }
-   
+
 
     // opcoes["a"] = letra ;
 }
 
 //var result = Opcao('a');
 
- console.log(opcoes);
+console.log(opcoes);
 //console.log(letra);
