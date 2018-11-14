bd8d51d27bccc5e7c65a6c1b70fad9950a53d527
gmatheus66 <gmatheus66@hotmail.com>
2018-11-14 13:17:47 -0300

    alteraçoes

--- Quiz²/index.js
+++ Quiz²/index.js
@@ -12,7 +12,7 @@ http.createServer(function(req, res){
             return res.end("ERROR 404");
         }
 
-        res.writeHead(200, {'Content-Type': 'text/html'});
+        res.writeHead(200);
         res.write(data);
         
         return res.end();
